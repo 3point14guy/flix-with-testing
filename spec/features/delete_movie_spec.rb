aa4db5fdd67042de4239c72c6cb7c7049f24910f
3poitn14guy <nicholas.piaskoski@gmail.com>
2018-10-26 16:51:50 -0400

    add test and code for deleting a resource

---
+++ spec/features/delete_movie_spec.rb
@@ -0,0 +1,14 @@
+require "rails_helper"
+
+describe "Deleting a movie" do
+  it "removes the movie from the db and shows an updated index page" do
+    movie = Movie.create(movie_attributes)
+
+    visit movie_path(movie)
+
+    click_link 'Delete'
+
+    expect(current_path).to eq(movies_path)
+    expect(page).not_to have_text(movie.title)
+  end
+end
