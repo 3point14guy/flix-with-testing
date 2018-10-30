ca81b9976f05fb0bd57ec5429c7b8635644171a0
3poitn14guy <nicholas.piaskoski@gmail.com>
2018-10-30 19:19:14 -0400

    fix tests, fix failing tests
    test for creating new movie still fails at route

--- spec/features/create_movie_spec.rb
+++ spec/features/create_movie_spec.rb
@@ -5,13 +5,13 @@ describe "creating a new movie" do
 
     visit movies_url
 
-    click_link "Add a Movie"
+    click_link "Add A Movie"
 
     expect(current_path).to eq(new_movie_path)
 
     fill_in "Title", with: "New Movie Title"
     fill_in "Description", with: "People doing stuff"
-    fill_in "Rating", with: "G"
+    select "PG-13", from: "movie_rating"
     fill_in "Total gross", with: "75000500"
     # select (Time.now.year - 1).to_s, :from => "movie_released_on_1i"
     fill_in "Released on", with: (Time.now.year - 1).to_s
@@ -34,109 +34,4 @@ describe "creating a new movie" do
 
   end
 
-  it "requires a title" do
-    movie = Movie.new(title: "")
-
-    movie.valid?  # populates errors
-
-    expect(movie.errors[:title].any?).to eq(true)
-  end
-
-  it "requires a description" do
-    movie = Movie.new(description: "")
-
-    movie.valid?
-
-    expect(movie.errors[:description].any?).to eq(true)
-  end
-
-  it "requires a description over 24 characters" do
-    movie = Movie.create(description: "X" * 24)
-
-    movie.valid?
-
-    expect(movie.errors[:description].any?).to eq(true)
-  end
-
-  it "requires a released on date" do
-    movie = Movie.new(released_on: "")
-
-    movie.valid?
-
-    expect(movie.errors[:released_on].any?).to eq(true)
-  end
-
-  it "requires a duration" do
-    movie = Movie.create(duration: "")
-
-    movie.valid?
-
-    expect(movie.errors[:duration].any?).to eq(true)
-  end
-
-  it "accepts a $0 total gross" do
-    movie = Movie.create(total_gross: 0)
-
-    movie.valid?
-
-    expect(movie.errors[:total_gross].any?).to eq(false)
-  end
-
-  it "accepts a positive total gross" do
-    movie = Movie.new(total_gross: 10000000.00)
-
-    movie.valid?
-
-    expect(movie.errors[:total_gross].any?).to eq(false)
-  end
-
-  it "rejects a negative total gross" do
-    movie = Movie.new(total_gross: -10000000.00)
-
-    movie.valid?
-
-    expect(movie.errors[:total_gross].any?).to eq(true)
-  end
-
-  it "accepts properly formatted image file names" do
-    file_names = %w[e.png movie.png movie.jpg movie.gif MOVIE.GIF]
-    file_names.each do |file_name|
-      movie = Movie.new(image_file_name: file_name)
-      movie.valid?
-      expect(movie.errors[:image_file_name].any?).to eq(false)
-    end
-  end
-
-  it "rejects improperly formatted image file names" do
-    file_names = %w[movie .jpg .png .gif movie.pdf movie.doc]
-    file_names.each do |file_name|
-      movie = Movie.new(image_file_name: file_name)
-      movie.valid?
-      expect(movie.errors[:image_file_name].any?).to eq(true)
-    end
-  end
-
-  it "accepts any rating that is in an approved list" do
-    ratings = %w[G PG PG-13 R NC-17]
-    ratings.each do |rating|
-      movie = Movie.new(rating: rating)
-      movie.valid?
-      expect(movie.errors[:rating].any?).to eq(false)
-    end
-  end
-
-  it "rejects any rating that is not in the approved list" do
-    ratings = %w[R-13 R-16 R-18 R-21]
-    ratings.each do |rating|
-      movie = Movie.new(rating: rating)
-      movie.valid?
-      expect(movie.errors[:rating].any?).to eq(true)
-    end
-  end
-
-  it "is valid with example attributes" do
-    movie = Movie.new(movie_attributes)
-
-    expect(movie.valid?).to eq(true)
-  end  
 end
