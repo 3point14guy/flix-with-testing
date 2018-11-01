1670046882008dab3ac2025566da0e93fc77a28e
3poitn14guy <nicholas.piaskoski@gmail.com>
2018-11-01 19:06:57 -0400

    refactor and style flash
    put flash in it's own partial
    create custom flash key
    add css for flash messages

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -42,7 +42,7 @@ class MoviesController < ApplicationController
   def destroy
     @movie = Movie.find(params[:id])
     @movie.destroy
-    redirect_to movies_url, notice: "Movie successfully deleted!"
+    redirect_to movies_url, danger: "Movie successfully deleted!"
   end
 
 private
