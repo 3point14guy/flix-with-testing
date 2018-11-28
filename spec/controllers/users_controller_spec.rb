372daf90c4884a21e9be45185adb79c01ef95479
3poitn14guy <nicholas.piaskoski@gmail.com>
2018-11-27 22:17:06 -0500

    finish user authorization
    add tests for wrong user
    add auth requirements for edit, update, destroy

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -41,6 +41,32 @@ describe UsersController do
 
       expect(response).to redirect_to(new_session_url)
     end
+  end
+
+  context "wrong user signed in" do
+
+    before do
+      @wrong_user = User.create!(user_attributes(email: "wrong@mail.com", username: "other"))
+      session[:user_id] = @wrong_user.id
+    end
+
+    it "cannot access edit" do
+      get :edit, params: { id: @user }
+
+      expect(response).to redirect_to(root_url)
+    end
+
+    it "cannot access update" do
+      patch :update, params: { id: @user }
+
+      expect(response).to redirect_to(root_url)
+    end
+
+    it "cannot access destroy" do
+      delete :destroy, params: { id: @user }
+
+      expect(response).to redirect_to(root_url)
+    end
 
   end
 end
